045f9584717d39e64d01802497bc916ce510d0ba
enixdark <cqshinn92@gmail.com>
2014-12-08 21:28:45 +0700

    update files

---
+++ nodejs_mongodb_example/collection_fuction/app.js
@@ -0,0 +1,3 @@
+/**
+ * Created by enixdark on 12/8/14.
+ */
---
+++ settings.js
@@ -0,0 +1,11 @@
+/**
+ * Created by enixdark on 12/8/14.
+ */
+
+exports.DB = {
+    host:'localhost',
+    port:27017,
+    username:'',
+    password:''
+    //database:'course'
+};
